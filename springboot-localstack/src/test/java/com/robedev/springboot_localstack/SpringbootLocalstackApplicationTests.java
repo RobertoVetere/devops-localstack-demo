20fb8b2d69aac4267bbfad118c565e1c06d3e153
Roberto Vetere <Roberto.vetere@gmail.com>
2025-03-11 17:11:40 +0100

    ADD 1.localstack

---
+++ springboot-localstack/src/test/java/com/robedev/springboot_localstack/SpringbootLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robedev.springboot_localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
